d80b1ebe164ca72cffaae7ca845cf0061787e3b9
itpark <a.m.smyslov@yandex.ru>
2023-01-06 14:24:21 +0300

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+
+    static void Log(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        Socket socket;
+
+        DataInputStream dataInputStream;
+        DataOutputStream dataOutputStream;
+
+        socket = new Socket(InetAddress.getByName("127.0.0.1"), 23465);
+        Log("connect to server");
+
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        System.out.print("Input info for server: ");
+        String msgToServer = scanner.nextLine();
+
+        dataOutputStream.writeUTF(msgToServer);
+        dataOutputStream.flush();
+
+        String msgFromServer = dataInputStream.readUTF();
+
+        System.out.println("msg from server: " + msgFromServer);
+
+        socket.close();
+
+        Log("disconnected from server");
+    }
+}
